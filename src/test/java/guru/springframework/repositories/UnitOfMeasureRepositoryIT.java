bf7758258d238e4504b960ce740671dffa94114b
akyare <akyare@hotmail.com>
2021-01-16 08:48:22 +0100

    Fixing failsafe plugin verify by converting UnitOfMeasureRepository Integration Test to JUNIT 5.

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -4,16 +4,18 @@ import guru.springframework.domain.UnitOfMeasure;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UnitOfMeasureRepositoryIT {
 
